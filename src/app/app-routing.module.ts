10ee5585b5aa01af768600de5574be939d400278
navinbarnwal <knbarnwal@gmail.com>
2020-12-12 02:41:23 +0530

    Made this for cready assesment, it has login from localstorage with listing out the list of movies on next page using the token saved in the localstorage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {IntroComponent} from './intro/intro.component';
+import {LoginComponent} from './login/login.component';
+import {MovieListComponent} from './movie-list/movie-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IntroComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'movie', component: MovieListComponent },
+  { path: 'intro', component: IntroComponent },
 
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { LoginComponent } from './login/login.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import {UserdetailsService} from './userdetails.service';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    LoginComponent,
+    MovieListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
